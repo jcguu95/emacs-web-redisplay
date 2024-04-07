5e1ee884fdee71032f9603d92b2b54b95bcdc6c0
jcguu95 <jcguu95@gmail.com>
2024-04-06 20:00:32 -0400

    emacs buffer strings now in electron
    Click the button in the page. Then the current buffer string that shows
    in the window gets piped to the webpage!

--- renderer.js
+++ renderer.js
@@ -10,10 +10,21 @@ windows.forEach((div, index) => {
 });
 
 btn.addEventListener('click', async () => {
-    const result = JSON.parse(JSON.parse(await window.electronAPI.click()))
-    console.log(result)
+    const output = await window.electronAPI.click()
+    console.log("output: ", output)
+    const result_ = JSON.parse(output)
+    console.log("result_: ", result_)
+    const result = JSON.parse(result_)
+    console.log("result: ", result) // A json list, each of whose entries is a json dict.
+    emacsWindow = document.querySelectorAll('.emacs-window')[0];
+    emacsWindow.textContent = '';
+    for (let i = 0; i < result.length; i++) {
+        span = document.createElement('span');
+        console.log(result[i]['text'])
+        span.textContent = result[i]['text'];
+        emacsWindow.appendChild(span);
+    }
 })
-
 
 // Add event listener for keyup event
 document.addEventListener('keyup', function(event) {
